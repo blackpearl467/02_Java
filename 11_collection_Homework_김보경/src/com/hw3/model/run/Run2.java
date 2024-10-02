230a263528a7e64aea048bbb52e55985f6d31bb0
blackpearl467 <kim991005@naver.com>
2024-10-02 17:46:45 +0900

    10월2일

---
+++ 11_collection_Homework_김보경/src/com/hw3/model/run/Run2.java
@@ -0,0 +1,11 @@
+package com.hw3.model.run;
+
+import com.hw3.model.dto.BookService2;
+
+public class Run2 {
+	public static void main(String[] args) {
+		
+		BookService2 service2 = new BookService2();
+		service2.displayMenu();
+}
+}
